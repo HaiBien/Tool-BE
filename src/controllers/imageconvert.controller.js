e9155dc9e107da9a08ab8619c97d6b0919185c80
HaiBien <lvh.bien@gmail.com>
2023-12-18 19:57:21 +0700

    #New

--- src/controllers/imageconvert.controller.js
+++ src/controllers/imageconvert.controller.js
@@ -1,4 +1,6 @@
 import sharp from "sharp";
+
+const fs = require('fs');
 import path from 'path';
 import upload_path from '../../uploads/_path';
 
@@ -8,13 +10,12 @@ require('dotenv').config();
 let ImageConverted = async (req, res) => {
   try {
     const {body} = req
-    console.log('body', body)
     const type = body.format;
     const resizedImages = [];
     for (const file of req.files) {
       const filePath = path.join(upload_path, file.filename)
       let resizedImage;
-      if(body.fit !== 'undefined') {
+      if (body.fit !== 'undefined') {
         resizedImage = await sharp(filePath)
           .resize(parseInt(body.width), parseInt(body.height), {fit: body.fit})
           .toFormat(body.format)
@@ -24,9 +25,12 @@ let ImageConverted = async (req, res) => {
           .toFormat(body.format)
           .toBuffer();
       }
-
       resizedImages.push({value: resizedImage, fileName: file.filename});
     }
+    req.files.forEach((file) => {
+      console.log('file', file)
+      fs.unlinkSync(path.join(upload_path, file.filename));
+    });
     return res.json({images: resizedImages, type})
   } catch (error) {
     console.log(error)
